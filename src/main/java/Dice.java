ad036b9e59097328419325965e00b63ee91b0fac
jgiroso <jrgiroso@gmail.com>
2021-07-18 23:14:24 -0400

    Dicey lab complete

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,25 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Dice {
 
+    int numOfDice;
 
+    public Dice(int numOfDice) {
+        this.numOfDice = numOfDice;
+    }
+
+    public Dice() {}
+
+    public int getNumberOfDice() {
+        return numOfDice;
+    }
+
+    public int tossAndSum() {
+        int sum = 0;
+        for(int i = 0; i < numOfDice; i++) {
+            sum += (Math.random() * 6) + 1;
+        }
+        return sum;
+    }
 }
