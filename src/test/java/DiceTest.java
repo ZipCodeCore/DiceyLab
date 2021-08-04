31b4d0f700a8016a8e2df6091407e3349500ad27
Raymond <raymond@zipcoder.local>
2021-08-04 11:04:35 -0400

    sorry i took so long

---
+++ src/test/java/DiceTest.java
@@ -0,0 +1,22 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DiceTest {
+    @Test
+    public void tossAndSumTest(){
+        // given
+        Dice dice = new Dice(2);
+
+        // when
+        dice.tossAndSum();
+
+        // then
+
+    }
+
+    }
+
+
