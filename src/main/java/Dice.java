041262e1508c3a88ee8a1e945bdc771a1e979438
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-11-20 11:37:58 -0500

    added Dice constructor and tossAndSum method

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,19 @@
+import java.util.Random;
+
 public class Dice {
+    private Integer numberOfDie;
 
+    public Dice(int numberOfDie) {
+        this.numberOfDie = numberOfDie;
+    }
 
+    public Integer tossAndSum() {
+        Random random = new Random();
+        Integer sum = 0;
+
+        for (int i = 0; i < numberOfDie; i++) {
+            sum += random.nextInt(6) + 1;
+        }
+        return sum;
+    }
 }
--- src/test/java/DiceTest.java
+++ src/test/java/DiceTest.java
@@ -1,9 +1,29 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 public class DiceTest{
 
     @Test
-    public void tossOneTest() {
+    public void tossAndSumTwoDiceTest() {
+        //Given
+        Dice dice = new Dice(2);
 
+        //When
+        Integer toss = dice.tossAndSum();
+
+        //Then
+        Assert.assertTrue((toss > 1) && (toss < 13));
+    }
+
+    @Test
+    public void tossAndSumFiveDiceTest() {
+        //Given
+        Dice dice = new Dice(5);
+
+        //When
+        Integer toss = dice.tossAndSum();
+
+        //Then
+        Assert.assertTrue((toss > 4) && (toss < 31));
     }
 }
